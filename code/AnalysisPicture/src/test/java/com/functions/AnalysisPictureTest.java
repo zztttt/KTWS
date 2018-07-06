1ac746c75bfb2796e21e617fcffd48f36e0c41f8
FJJLeon <695048014@qq.com>
2018-07-06 17:07:44 +0800

    update unit test and use python
    update analysis picture code and unit test, use python's pillow to mark face in picture (can not use except myself)

--- code/AnalysisPicture/src/test/java/com/functions/AnalysisPictureTest.java
+++ code/AnalysisPicture/src/test/java/com/functions/AnalysisPictureTest.java
@@ -1,10 +1,17 @@
 package com.functions;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import junit.framework.TestCase;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 public class AnalysisPictureTest extends TestCase {
 	
@@ -19,45 +26,100 @@ public class AnalysisPictureTest extends TestCase {
 		dataStrBuilder.append(dataStr);
 		System.out.println(dataStr);
 	}*/
-	
+	/*
 	@Test
 	public void testGetData() throws Exception {
-		String dataStr = ap.GetData(thisPath+"2.png");
+		String dataStr = ap.detectByPath(thisPath+"2.png");
 		//System.out.println(dataStrBuilder.toString());
 		assertFalse(dataStr.equals("picture not exists!"));
 		assertFalse(dataStr.equals("getdata fail"));
 	}
 
+	@Test
 	public void testNumOfFace() throws Exception {
-		String dataStr = ap.GetData(thisPath+"2.png");
+		String dataStr = ap.detectByPath(thisPath+"2.png");
 		System.out.println(dataStr);
-		int num = ap.NumOfFace(dataStr);
-		assertEquals(num,2);
+		int num = ap.numOfFace(dataStr);
+		System.out.println(num);
+		assertEquals(num,30);
+	}*/
+
+	public void testAnalysisFace() throws Exception {
+		String dataStr = ap.detectByPath("E:\\SummerProject\\photo\\4.jpg");
+		System.out.println(dataStr);
+		JSONObject dataJson = JSONObject.fromObject(dataStr);
+        JSONArray faceset = dataJson.getJSONArray("faces");
+        int count = faceset.size(); // number of faces
+        
+        String ftokens= "";
+        for (int i=0; i<count; i++) {
+        	JSONObject face = JSONObject.fromObject(faceset.get(i));
+        	String oneToken = face.get("face_token").toString();
+        	System.out.println(oneToken);
+        	if (i != 0) ftokens += ",";
+        	ftokens += oneToken;
+        }
+        // analysis face 
+        System.out.println(ftokens);
+        String res = ap.analysisFace(ftokens);
+    	System.out.println(res);
+    	assertFalse(res.equals("analysisface fail"));
+    	
+    	JSONObject resJson = JSONObject.fromObject(res);
+    	JSONArray resArray = resJson.getJSONArray("faces");
+    	int resCount = resArray.size();
+    	
+    	List<String> tmpList = new ArrayList<>();
+    	//tmpList.add("python");
+    	//tmpList.add("E:\\SummerProject\\pycode\\markPicture.py");
+    	String tmp = "E:\\python3\\python.exe E:\\SummerProject\\pycode\\markPicture.py";
+    	tmp += " E:\\SummerProject\\photo";
+    	tmp += " 4.jpg";
+    	//tmpList.add("E:\\SummerProject\\photo");
+    	//tmpList.add("4.jpg");
+    	for (int i=0;i<resCount;i++) {
+    		JSONObject face = JSONObject.fromObject(faceset.get(i));
+    		JSONObject rectangle = (JSONObject)face.get("face_rectangle");
+    		System.out.println(rectangle.toString());
+    		
+    		int top = Integer.parseInt(rectangle.get("top").toString());
+    		int left = Integer.parseInt(rectangle.get("left").toString());
+    		int width = Integer.parseInt(rectangle.get("width").toString());
+    		int height = Integer.parseInt(rectangle.get("height").toString());
+    		System.out.printf("left: %d, top:%d, width: %d, height: %d\n", left, top, width, height);
+    		
+    		//tmpList.add(String.valueOf(left));
+    		//tmpList.add(String.valueOf(top));
+    		//tmpList.add(String.valueOf(width));
+    		tmp += " "+left;
+    		tmp += " "+top;
+    		tmp += " "+width;
+    	}
+    	
+
+    	//String[] args = tmpList.toArray(new String[tmpList.size()]);
+    	System.out.println(tmp);
+    	
+    	try {
+    		Process proc = Runtime.getRuntime().exec(tmp);
+    		BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+    		String line = null;
+    		while ((line = in.readLine()) != null) {
+    			System.out.println(line);
+    		}
+    		in.close();
+	        int code = proc.waitFor();
+	        System.out.println("hhhh code: "+ code);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
 	}
 
-	public void testGet_facetoken() {
-		fail("Not yet implemented");
-	}
-
-	public void testCreate_faceset() {
-		fail("Not yet implemented");
-	}
-
-	public void testDelete_faceset() {
-		fail("Not yet implemented");
-	}
-
-	public void testAddface() {
-		fail("Not yet implemented");
-	}
-
-	public void testAnalysisface() {
-		fail("Not yet implemented");
-	}
-
+	/*
 	public void testGetBytesFromFile() {
 		fail("Not yet implemented");
 	}
+	*/
 
 
 }
