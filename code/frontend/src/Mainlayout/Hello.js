060c0acb23767ba60b9a77a6cb23626924e25efb
liqycarl <236778160@qq.com>
2018-07-09 15:03:25 +0800

    Charts
    implemention of the view of charts

--- code/frontend/src/Mainlayout/Hello.js
+++ code/frontend/src/Mainlayout/Hello.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 import Sidebar from '../Bars/Sidebar';
 import Headbar from '../Bars/Headbar';
 import '../Bars/Sidebar.css';
+var ReactHighcharts = require('react-highcharts');
+var Highcharts = require('highcharts');
+
+var config = {
+  title: {
+    text: '每日上课听讲人数'
+  },
+  subtitle: {
+    text: '课堂威视'
+  },
+  xAxis: {
+    title: {
+      text: 'Day'
+    },
+    categories: [
+      'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  tooltip: {
+    valueSuffix: '人'
+  },
+  legend: {
+    layout: 'vertical', 
+    align: 'right', 
+    verticalAlign: 'middle', 
+    borderWidth: 0
+  },
+  series: [
+    {
+      name: 'math', 
+      data: [7,8,9,11,2,5,6]
+    }, {
+      name: 'Chinese', 
+      data: [5,4,8,10,9,3,7]
+    }],
+}
 class Hello extends Component {
   render() {
     return (
@@ -11,7 +46,7 @@ class Hello extends Component {
           <Sidebar />
           <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
             <div>
-              <div>Hello</div>
+              <ReactHighcharts config={config}></ReactHighcharts>
             </div>
           </main>
         </div>
