8c0efc70ab1d48cf7d6ee931915f6b1d2514ef73
FJJLeon <695048014@qq.com>
2018-07-16 16:36:37 +0800

    update fe for props.username

--- code/frontend/src/Mainlayout/Chart.js
+++ code/frontend/src/Mainlayout/Chart.js
@@ -20,23 +20,26 @@ var classes = [{
 class Table extends React.Component {
   constructor(props){
     super(props);
-    this.getchart=this.getchart.bind(this);
-    this.getchart();
+    //this.getchart=this.getchart.bind(this);
+    this.state = {
+      classes: []
+    };
+    console.log(this.props.location.username);
+    this.serverRequest = $.post("/getclasses",{name:'111'},function(data){
+      console.log(data);
+      this.setState({
+           classes: JSON.parse(data),
+        });
+    }.bind(this));
+    console.log(this.state.classes);
+  }
 
-  }
-  getchart(){
-    this.serverRequest = $.post("/getclasses",{name:JSON.stringify(this.props.username)},function(data){
-          this.setState({
-               classes: JSON.parse(data),
-            });
-    })
-  }
   render() {
     /*const cellEditProp = {
       mode: 'dbclick',
     };*/
     return (
-      <BootstrapTable data={ classes } /*cellEdit={ cellEditProp }*/ striped={ true } search={ true } version='4'>
+      <BootstrapTable data={ this.state.classes } /*cellEdit={ cellEditProp }*/ striped={ true } search={ true } version='4'>
       <TableHeaderColumn dataField='id'  width={'10%'} isKey={ true }>课程号</TableHeaderColumn>
       <TableHeaderColumn dataField='classname'  width={'10%'}>课程名</TableHeaderColumn>
       <TableHeaderColumn dataField='num'  width={'10%'}>总人数</TableHeaderColumn>
