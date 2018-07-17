33da7b237a7e5e49c3cd517cc5a6eb38be843eb8
wzy <764994703@qq.com>
2018-07-17 15:26:31 +0800

    add antd table

--- code/frontend/src/Mainlayout/Chart.js
+++ code/frontend/src/Mainlayout/Chart.js
@@ -2,31 +2,64 @@ import React, { Component } from 'react';
 import Sidebar from '../Bars/Sidebar';
 import Headbar from '../Bars/Headbar';
 import $ from 'jquery';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { Table, Icon, Divider } from 'antd';
+import 'antd/dist/antd.css';
+import 'antd/lib/date-picker/style/css'; 
 var classes = [{
+      key:'1',
       id: 1,
       classname: "class1",
       num: 120,
       frequency: 5,
       open:'Y'
   }, {
+      key:'2',
       id: 2,
       classname: "class2", 
       num: 8,
       frequency: 20,
       open:'N'
   }];
+
+var columns=[{
+    title:'课程号',
+    dataIndex:'id',
+  },{
+    title:'课程名',
+    dataIndex:'classname',
+  },{
+    title:'总人数',
+    dataIndex:'num',
+  },{
+    title:'拍摄频率（秒）',
+    dataIndex:'frequency',
+  },{
+    title:'开启',
+    dataIndex:'open',
+  },{
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a href="javascript:;">Action{record.name}</a>
+        <Divider type="vertical" />
+        <a href="javascript:;">Delete</a>
+        <Divider type="vertical" />
+        <a href="javascript:;" className="ant-dropdown-link">
+          More actions <Icon type="down" />
+        </a>
+      </span>
+    ),
+  }];
 //const FrequencyTypes=[5,10,15,20];
-class Table extends React.Component {
+class Content extends React.Component {
   constructor(props){
     super(props);
     //this.getchart=this.getchart.bind(this);
     this.state = {
       classes: []
     };
-    console.log(this.props.location.username);
-    this.serverRequest = $.post("/getclasses",{name:'111'},function(data){
-      console.log(data);
+    this.serverRequest = $.post("/getclasses",{name:this.props.username},function(data){
       this.setState({
            classes: JSON.parse(data),
         });
@@ -39,14 +72,7 @@ class Table extends React.Component {
       mode: 'dbclick',
     };*/
     return (
-      <BootstrapTable data={ this.state.classes } /*cellEdit={ cellEditProp }*/ striped={ true } search={ true } version='4'>
-      <TableHeaderColumn dataField='id'  width={'10%'} isKey={ true }>课程号</TableHeaderColumn>
-      <TableHeaderColumn dataField='classname'  width={'10%'}>课程名</TableHeaderColumn>
-      <TableHeaderColumn dataField='num'  width={'10%'}>总人数</TableHeaderColumn>
-      <TableHeaderColumn dataField='frequency' /*editable={ { type: 'select', options:{ values: FrequencyTypes }}}*/width={'10%'}>拍照频率（秒）</TableHeaderColumn>
-      <TableHeaderColumn dataField='open' /*editable={ { type: 'checkbox', options: { values: 'Y:N' } } }*/ width={'10%'}>开启</TableHeaderColumn>
-      <TableHeaderColumn dataField='btn' width={'10%'}></TableHeaderColumn>      
-      </BootstrapTable>
+      <Table dataSource={classes} columns={columns} bordered></Table>
     );
   }
 }
@@ -68,7 +94,7 @@ class Chart extends Component {
           <Sidebar username={this.state.username}/>
           <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
             <div>
-              <Table username={this.state.username}/>
+              <Content username={this.state.username}/>
             </div>
           </main>
         </div>
