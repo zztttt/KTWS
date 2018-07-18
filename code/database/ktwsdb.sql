fd38b9708c1cf1be1d28b37f2f39938307ebfd08
FJJLeon <695048014@qq.com>
2018-07-18 09:52:41 +0800

    update database

--- code/database/ktwsdb.sql
+++ code/database/ktwsdb.sql
@@ -1,10 +1,11 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/7/16 11:02:13                           */
+/* Created on:     2018/7/18 9:47:52                            */
 /*==============================================================*/
 drop database if exists ktws;
 create database ktws;
 use ktws;
+
 
 drop table if exists Classroom;
 
@@ -32,7 +33,7 @@ create table Course
 (
    course_id            int not null,
    classroom_id         int not null,
-   id                   int,
+   user_id              int,
    course_name          varchar(20),
    total                int,
    time                 varchar(50),
@@ -45,7 +46,7 @@ create table Course
 create table Photo
 (
    photo_id             int not null,
-   classroom_id         int,
+   course_id            int,
    url                  varchar(100) not null,
    date                 date,
    total                int,
@@ -58,19 +59,19 @@ create table Photo
 /*==============================================================*/
 create table User
 (
-   id                   int not null auto_increment,
+   user_id              int not null auto_increment,
    password             varchar(20),
    role                 enum('ROLE_USER', 'ROLE_ADMIN'),
    username             varchar(20),
-   primary key (id)
+   primary key (user_id)
 );
 
 alter table Course add constraint FK_Arrangement foreign key (classroom_id)
       references Classroom (classroom_id) on delete restrict on update restrict;
 
-alter table Course add constraint FK_teach foreign key (id)
-      references User (id) on delete restrict on update restrict;
+alter table Course add constraint FK_teach foreign key (user_id)
+      references User (user_id) on delete restrict on update restrict;
 
-alter table Photo add constraint FK_shot foreign key (classroom_id)
-      references Classroom (classroom_id) on delete restrict on update restrict;
+alter table Photo add constraint FK_shot foreign key (course_id)
+      references Course (course_id) on delete restrict on update restrict;
 
