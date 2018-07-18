11be3053f903b3bb8f3348763bf2dfe5234387c9
zzt <1315691850@qq.com>
2018-07-18 16:36:40 +0800

    update Dao

--- code/securitySample/src/main/java/com/ktws/Dao/PhotoDao.java
+++ code/securitySample/src/main/java/com/ktws/Dao/PhotoDao.java
@@ -1,9 +1,12 @@
 package com.ktws.Dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ktws.Entity.Photo;
 
 public interface PhotoDao extends JpaRepository<Photo, Long> {
-
+	
+	@Query("select p.url from photo p where p.course_id = ?1")
+	String findUrlByCourseid(int id);
 }
