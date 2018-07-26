0eb2cc54fb1ed1f0e23742e183c21226c79b4530
liqycarl <236778160@qq.com>
2018-07-26 10:45:22 +0800

    welcome page
    welcome page

--- code/frontend/src/Mainlayout/Hello.js
+++ code/frontend/src/Mainlayout/Hello.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Sidebar from '../Bars/Sidebar';
 import Headbar from '../Bars/Headbar';
 import '../Bars/Sidebar.css';
+import '../Mainlayout/Hello.css';
 import $ from 'jquery';
 class Hello extends Component {
   constructor(props){
@@ -22,7 +23,9 @@ class Hello extends Component {
         <div className="row">
           <Sidebar/>
           <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-            <h1>欢迎使用课堂威视，{this.state.username}</h1>
+            <div className="hello">
+              <h1 className="artfont">欢迎使用课堂威视，{this.state.username}</h1>
+            </div>
           </main>
         </div>
       </div>
